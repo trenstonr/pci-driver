b30c9570aa72c5ae103aff74c5da676c44de83a7
keahi <trenstonricks@gmail.com>
2026-08-05 22:01:53 -0700

    Claim cpcidev PCI device

--- driver/device_driver.c
+++ driver/device_driver.c
@@ -6,3 +6,33 @@
 #define REG_OP2    0x14
 #define REG_OPCODE 0x18
 #define REG_RESULT 0x30
+
+#include <linux/pci.h>
+
+static struct pci_device_id id_table[] = {
+	{ PCI_DEVICE(CPCIDEV_VENDOR_ID, CPCIDEV_DEVICE_ID) },
+	{ 0, }
+};
+
+MODULE_DEVICE_TABLE(pci, id_table);
+
+static int probe (struct pci_dev *pdev, const struct pci_device_id *id) {
+	return pci_enable_device(pdev);
+}
+
+static void remove(struct pci_dev *pdev) {
+	pci_disable_device(pdev);
+}
+ 
+static struct pci_driver driva = {
+	.name = "super_cool_pci_device",
+	.id_table = id_table,
+	.probe = probe,
+	.remove = remove,
+};
+
+module_pci_driver(driva);
+
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("RANDOM DESCRIPTION");
